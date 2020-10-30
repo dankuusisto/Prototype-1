a3d3b704a9fb28b9f38d1c87f221e2aaf4f52897
Danimal <dankuusisto@gmail.com>
2020-10-30 10:15:16 -0400

    First commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward.
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Make the vehicle flutter around randomly
+        //transform.Translate(Random.Range(-1, 2), Random.Range(-1, 2), Random.Range(-1, 2));
+        
+    }
+}
